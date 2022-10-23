f8f3e2dd4fd04ac9359e67731b14d8542ed68514
sandipan898 <sandipan.das898@gmail.com>
2022-10-23 08:57:45 +0530

    dictionary contd

---
+++ dictionary(contd).py
@@ -0,0 +1,16 @@
+person_details = {
+    "fname": "Jhon",
+    "lname": "Doe"
+}
+
+# print(person_details)
+
+person_details["age"] = 22
+# print(person_details)
+
+person_details["designation"] = "developer"
+person_details["skills"] = ["python", "java", "javascript"]
+person_details["hobby"] = "painting"
+
+person_details["age"] = 25
+print(person_details)
